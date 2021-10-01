755f9928cb27e51cdb5ea7f0803d59391ff56326
Martina Di Girolamo <MARTINA.DI GIROLAMO@local.ittcesena.it>
2021-10-01 09:31:55 +0200

    V1.1: Aggiunto cicòlo

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -10,6 +10,21 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
+            int[] vettore = new int[5] { 3, 6, -3, 5, 7 };
+
+            int min = vettore[0];
+
+            for(int i=1;i<5;i++)
+            {
+                if(vettore[i]<min)
+                {
+                    min = vettore[i];
+                }
+
+                Console.WriteLine(min);
+
+                Console.ReadKey();
+            }
         }
     }
 }
